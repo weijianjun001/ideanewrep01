e9bc9d77cd905b322f7671f980eaa32aee922587
weijianjun <1281514787@qq.com>
2018-12-26 22:18:57 +0800

    FirstGit第一次提交

---
+++ firstgit/src/com/baidu/service/TestGit.java
@@ -0,0 +1,12 @@
+package com.baidu.service;
+
+public class TestGit {
+    public static void main(String[] args) {
+
+        System.out.println("git来啦");
+        System.out.println("git来啦");
+        System.out.println("git来啦");
+        System.out.println("git来啦");
+        System.out.println("git来啦");
+    }
+}
